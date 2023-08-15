49d1db20ab727606473546d790ce96c76d7bd014
monsieurkd <87173358+monsieurkd@users.noreply.github.com>
2023-08-15 15:52:06 +0930

    sd

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -7,7 +7,7 @@ int *readNumbers()
     int *arr = new int(10);
     for (int i = 0; i < 10; i++)
     {
-        cout << "enter the number for " << i + 1 << " ";
+        
         cin >> arr[i];
     }
     return arr;
--- function-4-1.cpp
+++ function-4-1.cpp
@@ -6,7 +6,7 @@ int  x;
 int *arr = new int(10);
 for (int i = 0; i < 10; i++)
 {
-    cout << "enter the number for " << i+1 << " " ;
+  
     cin>>arr[i];
 }
 return arr;
