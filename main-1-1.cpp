4b2f864f422227b8ec6230f9a42e9c9bd18ad388
monsieurkd <87173358+monsieurkd@users.noreply.github.com>
2023-09-08 17:39:26 +0930

    d

---
+++ main-1-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "AirVehicle.h"
+#include "Airplane.h"
+#include "Helicopter.h"
+#include "AirFleet.h"
+#include <string>
+using namespace std;
+
+int main(){
+    return 0;
+}
